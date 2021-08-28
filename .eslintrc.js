e8e84a13237d501be86cdd4d16780ee2f646bd7f
Gilson.Conceicao <gilson.conceicao@al.infnet.edu.br>
2021-08-28 15:18:06 -0300

    Commit Initial

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended'
+  ],
+  plugins: [
+  ],
+  // add your custom rules here
+  rules: {
+
+    'no-trailing-spaces': ['warn', { skipBlankLines: true }]
+
+  }
+}
