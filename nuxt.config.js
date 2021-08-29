102647f71b1ea556d6e643d93975e5eb4977982c
Gilson.Conceicao <gilson.conceicao@al.infnet.edu.br>
2021-08-29 16:09:18 -0300

    added router base for gh-pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
+  router: {
+    base: '/hellonuxtjs/'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'hellonuxtjs',
