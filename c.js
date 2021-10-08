23190ac3b9f1c83c078374a2b5c7991adb8b4b86
Gilson.Conceicao <gilson.conceicao@al.infnet.edu.br>
2021-10-08 04:47:26 -0300

    Features novas

--- c.js
+++ c.js
@@ -5,4 +5,30 @@ const cgtFomObj = key => ({
   'c-c': 'valor de c-c'
 })[key] || 'Key inexistente'
 
-console.log(cgtFomObj('c-c')) 
+const bla = (par, { objPar2, objPar }) => {
+  return { par, objPar, objPar2 }
+}
+
+const obj = {
+  objPar: 'OBJPV',
+  objPar2: 'OBJPV2'
+}
+
+console.log(bla(1, obj))
+
+async function loadStickyTopic () {
+  try {
+    const response = await apiService.get('topics', {
+      params: {
+        page: {
+          number: 1,
+          size: 1
+        }
+      }
+    })
+
+    commit('setStickTopic', response.data.data[0])
+  } catch (error) {
+    throw new Error('Não foi possível carregar o tópico fixo')
+  }
+}
